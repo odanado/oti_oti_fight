52318a77f865890773bd9fa93290f4d8167a83bd
odan <odan3240@gmail.com>
2016-07-15 09:28:20 +0900

    SceneBaseを分割した

--- include/SceneManager.h
+++ include/SceneManager.h
@@ -12,32 +12,9 @@
 #include <functional>
 #include <boost/optional.hpp>
 
+#include "SceneBase.h"
+
 namespace oti_oti_fight {
-
-template <class State, class Data>
-class SceneManager;
-
-template <typename State, class Data>
-class SceneBase {
- public:
-    using Manager_t = SceneManager<State, Data>;
-
-    virtual void init() noexcept = 0;
-    virtual void update() noexcept = 0;
-    virtual void draw() noexcept = 0;
-
-    void setData(Manager_t *manager, const std::shared_ptr<Data> &data) {
-        this->manager = manager;
-        this->data = data;
-    }
-
- protected:
-    std::shared_ptr<Data> data;
-    bool changeScene(const State &state) { return manager->changeScene(state); }
-
- private:
-    Manager_t *manager = nullptr;
-};
 
 template <class State, class Data = void>
 class SceneManager {
