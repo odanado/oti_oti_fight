44b65e5cfdcaef6481da647b8858cef77e21e67f
odan <odan3240@gmail.com>
2016-07-15 09:40:01 +0900

    Add GameData

---
+++ include/GameData.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2016 odanado
+ * Licensed under the MIT License.
+ */
+
+#ifndef INCLUDE_GAMEDATA_H_
+#define INCLUDE_GAMEDATA_H_
+
+#include "Board.h"
+
+namespace oti_oti_fight {
+struct GameData {
+    Board board;
+};
+}  // namespace oti_oti_fight
+#endif  // INCLUDE_GAMEDATA_H_
