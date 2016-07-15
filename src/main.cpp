b5f769f9910da87084c3d4eb3139f9727053ab38
odan <odan3240@gmail.com>
2016-07-15 10:34:09 +0900

    フレーム制御するようにmain.cppを修正した

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,31 @@
  * Licensed under the MIT License.
  */
 
+#include <chrono>
+#include <thread>
+
 #include "SceneManager.h"
 #include "TitleScene.h"
 
 int main() {
+    using std::chrono::system_clock;
+    using std::literals::chrono_literals::operator""us;
+
     oti_oti_fight::MyApp manager;
     manager.add<oti_oti_fight::TitleScene>("Title");
+
+    auto nextFrame = system_clock::now() + 16666us;
     while (1) {
-        manager.updateAndDraw();
+        manager.update();
+        auto nowFrame = system_clock::now();
+        if (nowFrame < nextFrame) {
+            manager.draw();
+            nowFrame = system_clock::now();
+            if (nowFrame < nextFrame) {
+                std::this_thread::sleep_for(nextFrame - nowFrame);
+            }
+        }
+
+        nextFrame += 16666us;
     }
 }
